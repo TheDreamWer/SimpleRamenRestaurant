d32c835493814a306836a157cbbe7e8b7c25f9a0
zwc2074 <48357418+zwc2074@users.noreply.github.com>
2020-04-09 22:35:32 +0800

    Update Controller.java
    add actionlistener to btnMenus

--- src/boundary/Controller.java
+++ src/boundary/Controller.java
@@ -68,7 +68,7 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Node[] nodes = new Node[customer.getOrderList().Getter().size()];
+        Node[] nodes = new Node[customer.getOrderList().getArraylist().size()];
         int i = 0;
         for (i = 0; i < nodes.length; i++) {
             try {
@@ -81,9 +81,9 @@ public class Controller implements Initializable {
                 Label name = (Label) p.lookup("#UserName");
                 Label time = (Label) p.lookup("#Time");
                 Button active = (Button) p.lookup("#Active");
-                id.setText("" + customer.getOrderList().Getter().get(i).getOrderID());
-                name.setText("" + customer.getOrderList().Getter().get(i).getCode());
-                time.setText("" + customer.getOrderList().Getter().get(i).getGenerateTime());
+                id.setText("" + customer.getOrderList().getArraylist().get(i).getOrderID());
+                name.setText("" + customer.getOrderList().getArraylist().get(i).getCode());
+                time.setText("" + customer.getOrderList().getArraylist().get(i).getGenerateTime());
 
                 //give the items some effect
                 nodes[i].setOnMouseEntered(event -> {
@@ -94,7 +94,7 @@ public class Controller implements Initializable {
                 });
                 pnItems.getChildren().add(nodes[i]);
 
-                //System.out.println(customer.getOrderList().Getter().get(i).getOrderID());
+                //System.out.println(customer.getOrderList().getArraylist().get(i).getOrderID());
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -117,13 +117,13 @@ public class Controller implements Initializable {
             pnlOverview.toFront();
             //setOnAction
             Label totalOrders = (Label)pnlOverview.lookup("#TotalOrders");
-            totalOrders.setText("" + customer.getOrderList().Getter().size());
+            totalOrders.setText("" + customer.getOrderList().getArraylist().size());
 
             Label totalAmount = (Label)pnlOverview.lookup("#TotalAmount");
-            totalAmount.setText("" + customer.getOrderList().Getter().size());
+            totalAmount.setText("" + customer.getOrderList().getArraylist().size());
 
             Label todayOrders = (Label)pnlOverview.lookup("#TodayOrders");
-            todayOrders.setText("" + customer.getOrderList().Getter().size());
+            todayOrders.setText("" + customer.getOrderList().getArraylist().size());
         }
         if(actionEvent.getSource()==btnOrders)
         {
@@ -168,6 +168,263 @@ public class Controller implements Initializable {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+        }
+        if(actionEvent.getSource()==btnMenus)
+        {
+            Label Amount = (Label) pnlMenus.lookup("#Amount");
+            //初始化Ramen
+            ArrayList<Ramen> tempRamen = new ArrayList<Ramen>();
+            tempRamen.add(new Ramen());
+            tempRamen.get(tempRamen.size()-1).setOnion_level(-1);
+
+            //额外的Nori 增减部分
+            Label ExtraNori = (Label) pnlMenus.lookup("ExtraNori");
+            Button addNori = (Button) pnlMenus.lookup("#addNori");
+            addNori.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    ExtraNori.setText(String.valueOf(Integer.parseInt(ExtraNori.getText())+1));
+                    Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())+1));
+                    tempRamen.get(tempRamen.size()-1).setExtra_nori(Integer.parseInt(ExtraNori.getText())+1);
+                }
+            });
+            Button subNori = (Button) pnlMenus.lookup("#subNori");
+            subNori.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (!ExtraNori.getText().equals("0"))
+                        ExtraNori.setText(String.valueOf(Integer.parseInt(ExtraNori.getText())-1));
+                        Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())-1));
+                        tempRamen.get(tempRamen.size()-1).setExtra_nori(Integer.parseInt(ExtraNori.getText())-1);
+                }
+            });
+
+            //额外的Chashu 增减
+            Label ExtraChashu = (Label) pnlMenus.lookup("ExtraChashu");
+            Button addChashu = (Button) pnlMenus.lookup("#addChashu");
+            addChashu.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    ExtraChashu.setText(String.valueOf(Integer.parseInt(ExtraChashu.getText())+2));
+                    Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())+2));
+                    tempRamen.get(tempRamen.size()-1).setExtra_chashu(Integer.parseInt(ExtraNori.getText())+2);
+                }
+            });
+            Button subChashu = (Button) pnlMenus.lookup("#subChashu");
+            subChashu.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (!ExtraChashu.getText().equals("0"))
+                        ExtraChashu.setText(String.valueOf(Integer.parseInt(ExtraChashu.getText())-2));
+                        Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())-2));
+                }
+            });
+
+            //额外的BoiledEgg 增减
+            Label ExtraBoiledEgg = (Label) pnlMenus.lookup("ExtraBoiledEgg");
+            Button addBoiledEgg = (Button) pnlMenus.lookup("#addBoiledEgg");
+            addBoiledEgg.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    ExtraBoiledEgg.setText(String.valueOf(Integer.parseInt(ExtraBoiledEgg.getText())+1));
+                    Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())+1));
+                    tempRamen.get(tempRamen.size()-1).setExtra_boiled_egg(Integer.parseInt(ExtraNori.getText())+1);
+                }
+            });
+            Button subBoiledEgg = (Button) pnlMenus.lookup("#subBoiledEgg");
+            subBoiledEgg.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (!ExtraBoiledEgg.getText().equals("0"))
+                        ExtraBoiledEgg.setText(String.valueOf(Integer.parseInt(ExtraBoiledEgg.getText())-1));
+                        Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())-1));
+                        tempRamen.get(tempRamen.size()-1).setExtra_boiled_egg(Integer.parseInt(ExtraNori.getText())-1);
+                }
+            });
+
+            //额外 Bamboo shoot 的增减
+            Label BambooShoots = (Label) pnlMenus.lookup("BambooShoots");
+            Button addBambooShoot = (Button) pnlMenus.lookup("#addBambooShoot");
+            addBambooShoot.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    BambooShoots.setText(String.valueOf(Integer.parseInt(BambooShoots.getText())+1));
+                    Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())+1));
+                    tempRamen.get(tempRamen.size()-1).setBamboo_shoots(Integer.parseInt(ExtraNori.getText())+1);
+                }
+            });
+            Button subBambooShoot = (Button) pnlMenus.lookup("#subBambooShoot");
+            subBambooShoot.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (!BambooShoots.getText().equals("0"))
+                        BambooShoots.setText(String.valueOf(Integer.parseInt(BambooShoots.getText())-1));
+                    Amount.setText(String.valueOf(Integer.parseInt(Amount.getText())-1));
+                    tempRamen.get(tempRamen.size()-1).setBamboo_shoots(Integer.parseInt(ExtraNori.getText())-1);
+                }
+            });
+
+            //面汤group
+            ToggleGroup groupSoup = new ToggleGroup();
+            RadioButton Tonkotsu = (RadioButton) pnlMenus.lookup("#Tonkotsu");
+            Tonkotsu.setToggleGroup(groupSoup);
+            RadioButton Shoyu = (RadioButton) pnlMenus.lookup("#Shoyu");
+            Shoyu.setToggleGroup(groupSoup);
+            RadioButton Shio = (RadioButton) pnlMenus.lookup("#Shio");
+            Shio.setToggleGroup(groupSoup);
+            groupSoup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                        public void changed(ObservableValue<? extends Toggle> changed,
+                                            Toggle oldVal, Toggle newVal) {
+                            if (groupSoup.getSelectedToggle() != null) {
+                                tempRamen.get(tempRamen.size()-1).setSoup(groupSoup.getSelectedToggle().toString());
+                            }
+                        }
+                    });
+
+            //面的group
+            ToggleGroup groupNoddles = new ToggleGroup();
+            RadioButton NoodleSoft = (RadioButton) pnlMenus.lookup("#NoodleSoft");
+            NoodleSoft.setToggleGroup(groupNoddles);
+            RadioButton NoodleMedium = (RadioButton) pnlMenus.lookup("#NoodleMedium");
+            NoodleMedium.setToggleGroup(groupNoddles);
+            RadioButton NoodleFirm = (RadioButton) pnlMenus.lookup("#NoodleFirm");
+            NoodleFirm.setToggleGroup(groupNoddles);
+            groupNoddles.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupNoddles.getSelectedToggle() != null) {
+                        tempRamen.get(tempRamen.size()-1).setNoodles(groupNoddles.getSelectedToggle().toString());
+                    }
+                }
+            });
+
+            //洋葱 group
+            ToggleGroup groupOnion = new ToggleGroup();
+            RadioButton OnionNo = (RadioButton) pnlMenus.lookup("#OnionNo");
+            OnionNo.setToggleGroup(groupOnion);
+            RadioButton OnionLittle = (RadioButton) pnlMenus.lookup("#OnionLittle");
+            OnionLittle.setToggleGroup(groupOnion);
+            RadioButton OnionLot = (RadioButton) pnlMenus.lookup("#OnionLot");
+            OnionLot.setToggleGroup(groupOnion);
+            groupOnion.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupOnion.getSelectedToggle() != null) {
+                        if (groupOnion.getSelectedToggle().getUserData().equals("NO"))
+                            tempRamen.get(tempRamen.size()-1).setOnion_level(0);
+                        else if (groupOnion.getSelectedToggle().getUserData().equals("NO"))
+                            tempRamen.get(tempRamen.size()-1).setOnion_level(1);
+                        else
+                            tempRamen.get(tempRamen.size()-1).setOnion_level(2);
+                    }
+                }
+            });
+
+            //Nori 的group
+            ToggleGroup groupNori = new ToggleGroup();
+            RadioButton NoriYes = (RadioButton) pnlMenus.lookup("#NoriYes");
+            NoriYes.setToggleGroup(groupNori);
+            RadioButton NoriNo = (RadioButton) pnlMenus.lookup("#NoriNo");
+            NoriNo.setToggleGroup(groupNori);
+            groupNori.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupNori.getSelectedToggle() != null) {
+                        if (groupNori.getSelectedToggle().getUserData().equals("Yes")) {
+                            addNori.setDisable(false);      //根据选择来判断是否需要禁用相关button
+                            subNori.setDisable(false);
+                            tempRamen.get(tempRamen.size()-1).setNori(true);
+                        }
+                        else {
+                            addNori.setDisable(true);
+                            subNori.setDisable(true);
+                            tempRamen.get(tempRamen.size()-1).setNori(false);
+                        }
+                    }
+                }
+            });
+
+            //chashu 的group
+            ToggleGroup groupchashu = new ToggleGroup();
+            RadioButton chashuYes = (RadioButton) pnlMenus.lookup("#chashuYes");
+            chashuYes.setToggleGroup(groupchashu);
+            RadioButton chashuNo = (RadioButton) pnlMenus.lookup("#chashuNo");
+            NoriNo.setToggleGroup(groupchashu);
+            groupNori.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupchashu.getSelectedToggle() != null) {
+                        if (groupchashu.getSelectedToggle().getUserData().equals("Yes")) {
+                            addChashu.setDisable(false);      //根据选择来判断是否需要禁用相关button
+                            subChashu.setDisable(false);
+                            tempRamen.get(tempRamen.size()-1).setChashu(true);
+                        }
+                        else {
+                            addChashu.setDisable(true);
+                            subChashu.setDisable(true);
+                            tempRamen.get(tempRamen.size()-1).setChashu(false);
+                        }
+                    }
+                }
+            });
+
+            //煮鸡蛋的group
+            ToggleGroup groupBoiledEgg = new ToggleGroup();
+            RadioButton BoiledEggYes = (RadioButton) pnlMenus.lookup("#BoiledEggYes");
+            chashuYes.setToggleGroup(groupBoiledEgg);
+            RadioButton BoiledEggNo = (RadioButton) pnlMenus.lookup("#BoiledEggNo");
+            NoriNo.setToggleGroup(groupBoiledEgg);
+            groupNori.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupBoiledEgg.getSelectedToggle() != null) {
+                        if (groupBoiledEgg.getSelectedToggle().getUserData().equals("Yes")) {
+                            addBoiledEgg.setDisable(false);      //根据选择来判断是否需要禁用相关button
+                            subBoiledEgg.setDisable(false);
+                            tempRamen.get(tempRamen.size()-1).setBoiled_egg(true);
+                        }
+                        else {
+                            addBoiledEgg.setDisable(true);
+                            subBoiledEgg.setDisable(true);
+                            tempRamen.get(tempRamen.size()-1).setBoiled_egg(false);
+                        }
+                    }
+                }
+            });
+
+            //辣度的group
+            ToggleGroup groupSpiciness = new ToggleGroup();
+            RadioButton[] rSpiciness = new RadioButton[6];
+            for (int i=0;i<6;i++) {
+                rSpiciness[i] = (RadioButton) pnlMenus.lookup("Spiciness"+i);       //Radiobutton的id需为Spiciness加后缀1～5
+                rSpiciness[i].setToggleGroup(groupSpiciness);
+            }
+            groupSpiciness.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                public void changed(ObservableValue<? extends Toggle> changed,
+                                    Toggle oldVal, Toggle newVal) {
+                    if (groupSpiciness.getSelectedToggle() != null) {
+                        tempRamen.get(tempRamen.size()-1).setSpiciness(Integer.parseInt(
+                                groupSpiciness.getSelectedToggle().getUserData().toString()));
+                    }
+                }
+            });
+
+            //添加购物车
+            Button addCart = (Button) pnlMenus.lookup("#addcart");
+            addCart.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    if ((!tempRamen.get(tempRamen.size()-1).getSoup().isEmpty())
+                            &&(!tempRamen.get(tempRamen.size()-1).getNoodles().isEmpty())
+                            &&tempRamen.get(tempRamen.size()-1).getOnion_level()!=-1) {
+                        customer.getDraft().getRamenList().add(tempRamen.get(tempRamen.size()-1));
+                        customer.getDraft().setOrderAmount(customer.getDraft().getOrderAmount()
+                                + tempRamen.get(tempRamen.size()-1).calcRamenAmount());
+                        tempRamen.add(new Ramen());
+                    }
+                }
+            });
 
         }
     }
