71fa55eeea40b39ed4b47c605c18b68b952ee32c
Ricebucket <33026414+Ricebucketcn@users.noreply.github.com>
2020-04-20 17:26:41 +0800

    Update Ticket.java

--- src/entities/Ticket.java
+++ src/entities/Ticket.java
@@ -5,11 +5,12 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.Serializable;
 
+import control.SystemTime;
 import entities.*;
 
 public class Ticket implements Serializable{
     /**
-     *Iteration 1
+     *Iteration 2
      */
     private static final long serialVersionUID = 1L;
     private Order order;
@@ -25,18 +26,24 @@ public class Ticket implements Serializable{
         this.order = order;
     }
 
-    public void printTicket() { //--------------还需要修改------------   
+    public void printTicket() { 
         try {
 			File fl = new File("data/ticket.txt");
             FileWriter fw = new FileWriter(fl);
             BufferedWriter bw = new BufferedWriter(fw);
-            String titleline = 
-                "order_ID,code,order_amount,payment_method,generateTime,soup,noodles,onion_level,nori,chashu,boiled_egg,spiciness,extra_nori,extra_boiled_egg,Bamboo_shoots,extra_chashu\n";
-            bw.write(titleline);
+            SystemTime systemTime = new SystemTime();
+            String time = systemTime.getDateString();
+            String intro = "Werlcome to Totoro Ramen!\n";
+            intro += "\nDate: " + time;
+            intro += "\nOrder ID: " + this.order.getOrderID();
+            intro += "\nOrder Amount: " + this.order.getOrderAmount();
+            intro += "\nCode: " + this.order.getCode();
+            intro += "\n=================================\n";
+            bw.write(intro);
 			bw.flush();
             for(int i=0; i<this.order.getRamenList().size(); i++){
-                String order_string = this.order.orderToString(i) + "\n";
-                bw.write(order_string);
+                Ramen ramen = this.order.getRamenList().get(i);
+                bw.write(printRamen(ramen, i));
                 bw.flush();
             } 
 			bw.close();
@@ -46,5 +53,21 @@ public class Ticket implements Serializable{
 			System.err.println("Class Restaurant Constructor Error");
 			e.printStackTrace();
 		}
+    }
+
+    public String printRamen(Ramen ramen, int count){
+        String ramenInfo = "---------------  "+ count +"  --------------";
+        ramenInfo += "\nSoup: " + ramen.getSoup();
+        ramenInfo += "\nNoodles: " + ramen.getNoodles();
+        ramenInfo += "\nOnion Level: : " + ramen.getOnion_level();
+        ramenInfo += "\nBamboo Shoots: " + ramen.getBamboo_shoots();
+        ramenInfo += "\nNori : " + ramen.getNori() + "  Extra: " + ramen.getExtra_nori();
+        ramenInfo += "\nChashu: " + ramen.getChashu() + "  Extra: " + ramen.getExtra_chashu();
+        ramenInfo += "\nBoiled Egg: " + ramen.getBoiled_egg() + "   Extra: " + ramen.getExtra_boiled_egg();
+        ramenInfo += "\nSpiciness: " + ramen.getSpiciness();
+        ramenInfo += "\nNumber : " + ramen.getNum(); 
+        ramenInfo += "\n---------------------------------\n\n";
+
+        return ramenInfo;
     }
 }
