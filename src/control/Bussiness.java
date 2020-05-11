fde9a8cb7577bddd939e9e63ae4c6cd266fd968d
baihaoran <50100285+BUPTggbang@users.noreply.github.com>
2020-05-11 16:32:43 +0800

    Update Bussiness.java

--- src/control/Bussiness.java
+++ src/control/Bussiness.java
@@ -146,57 +146,7 @@ public class Bussiness {
     }
 
 
-
-
-
-
-
-
-    //显示所有订单信息
-    public ArrayList<String> showoder() {
-        ArrayList<Order> order ;//创建一个order数组引用变量
-        OrderList list= new OrderList();
-        list=list.getter(); //得到返回的OrderList
-        order =list.Getter();//得到返回的ArrayList
-
-
-        ArrayList<String> order_info = new ArrayList<>(); //储存order信息String形式
-        String text="";
-        //打印Order信息
-        for (int i = 0; i < order.size(); i++) {
-            text="";
-            text+="******Basic Information******\n";
-            text+="OrderID:"+String.valueOf(order.get(i).getOrderID())+"\n";//订单ID
-            text+="Code:"+String.valueOf(order.get(i).getCode())+"\n";//订单取餐码
-            text+="OrderAmount:"+String.valueOf(order.get(i).getOrderAmount())+"\n";//订单amount
-            text+="PaymentMethod:"+String.valueOf(order.get(i).getPaymentMethod())+"\n";//付款方式
-            text+="Time:"+String.valueOf(order.get(i).getGenerateTime())+"\n\n";//订单生成时间
-
-
-            ArrayList<Ramen> Ramenlist ; //创建Ramen数组，用于保存每个订单的ramen信息
-            Ramenlist = order.get(i).getRamenList();
-            for (int j = 0; j < Ramenlist.size();j++) {
-                text+="******Ramen Option******\n";
-                //打印Ramen选项
-                text+="Soup:" +String.valueOf(Ramenlist.get(j).getSoup())+"\n";
-                text+="Noodles:" +String.valueOf(Ramenlist.get(j).getNoodles())+"\n";
-                text+="Onion_level:" +String.valueOf(Ramenlist.get(j).getOnion_level())+"\n";
-                text+="Nori:" +String.valueOf(Ramenlist.get(j).getNori())+"\n";
-                text+="Chashu:" +String.valueOf(Ramenlist.get(j).getChashu())+"\n";
-                text+="Boiled_egg:" +String.valueOf(Ramenlist.get(j).getBoiled_egg())+"\n";
-                text+="Spiciness:" +String.valueOf(Ramenlist.get(j).getSpiciness())+"\n\n";
-
-                text+="******Ramen Extra Option******\n";
-                //打印Ramen额外添加项
-                text+="Extra_nori:" +String.valueOf(Ramenlist.get(j).getExtra_nori())+"\n";
-                text+="Extra_boiled_egg:" +String.valueOf(Ramenlist.get(j).getExtra_boiled_egg())+"\n";
-                text+="Extra_chashu:" +String.valueOf(Ramenlist.get(j).getExtra_chashu())+"\n";
-                text+="Bamboo_shoots:" +String.valueOf(Ramenlist.get(j).getBamboo_shoots())+"\n";
-            }
-            order_info.add(text);
-        }
-        return order_info;
-    }
+    
 
 
 
